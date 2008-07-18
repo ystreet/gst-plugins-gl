3a87bcd4c1656bdefc30e19ed60dbbf07d58f26c
Julien Isorce <julien.isorce@gmail.com>
2008-07-18 21:55:00 +0200

    add GstGLShader stuffs in the vs8 project, fix laplacian fragment code to make it compile on ATI, and fix check function about Opengl and Glew version

--- tests/examples/qglwidgetxoverlay/qglrenderer.cpp
+++ tests/examples/qglwidgetxoverlay/qglrenderer.cpp
@@ -1,9 +1,19 @@
 #include "qglrenderer.h"
 
-QGLRenderer::QGLRenderer(QWidget *parent)
-    : QGLWidget(parent)
+QGLRenderer::QGLRenderer(const QString videoLocation, QWidget *parent)
+    : QGLWidget(parent),
+    m_gt(winId(), videoLocation)
 {
     setAttribute(Qt::WA_NoSystemBackground);
+    setVisible(false);
+    move(20, 10);
+    resize(640, 480);
+
+    QObject::connect(&m_gt, SIGNAL(finished()), this, SLOT(close()));
+    QObject::connect(this, SIGNAL(exposeRequested()), &m_gt, SLOT(exposeRequested()));
+    QObject::connect(this, SIGNAL(closeRequested()), &m_gt, SLOT(stop()), Qt::DirectConnection);
+    QObject::connect(&m_gt, SIGNAL(showRequested()), this, SLOT(show()));
+    m_gt.start();
 }
 
 QGLRenderer::~QGLRenderer()
@@ -15,3 +25,8 @@ void QGLRenderer::paintEvent(QPaintEvent* event)
     emit exposeRequested();
 }
 
+void QGLRenderer::closeEvent(QCloseEvent* event)
+{
+    emit closeRequested();
+    m_gt.wait();
+}
--- tests/examples/qglwidgetxoverlay/qglrenderer.h
+++ tests/examples/qglwidgetxoverlay/qglrenderer.h
@@ -2,18 +2,24 @@
 #define QGLRENDERER_H
 
 #include <QGLWidget>
+#include "gstthread.h"
 
 class QGLRenderer : public QGLWidget
 {
     Q_OBJECT
 
 public:
-    QGLRenderer(QWidget *parent = 0);
+    QGLRenderer(const QString videoLocation, QWidget *parent = 0);
     ~QGLRenderer();
     void paintEvent(QPaintEvent* event);
+    void closeEvent (QCloseEvent* event);
 
 signals:
     void exposeRequested();
+    void closeRequested();
+
+private:
+    GstThread m_gt;
 };
 
 #endif // QGLRENDERER_H
